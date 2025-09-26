a49eed124952865d55387b9f502b7faf1b78b36a
Caroline Duarte <caroline_lima.duarte@outlook.com>
2025-09-26 16:55:10 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,194 @@
+#include <stdio.h>
 
+int main (){
+
+char codigo_carta [5],nome_da_cidade[40],estado [50];
+int populacao , pontos_turisticos ;
+float area_em_km , pib ;
+
+printf ("\nVamos a PRIMEIRA carta do SUPER TRUNFO! \n");
+
+    printf ("\nDigite o nome do estado: \n");
+    scanf ("%s", &estado);
+
+    printf ("Digite o código da carta: \n");
+    scanf ("%s" , &codigo_carta);
+
+    printf ("Digite o nome da cidade: \n");
+    scanf ("%s" , &nome_da_cidade);
+
+    printf ("Digite a população da cidade: \n");
+    scanf ("%d" , &populacao);
+
+    printf ("Digite a área da cidade em km²: \n"); 
+    scanf("%f", &area_em_km);
+
+    printf ("Digite o PIB da cidade: \n");
+    scanf ("%f", &pib);
+
+    printf("Digite o número de pontos turisticos da cidade: \n");
+    scanf ("%d" ,&pontos_turisticos);
+
+char codigo_carta2 [5],nome_da_cidade2 [40],estado2 [50];
+int populacao2,pontos_turisticos2;
+float area_em_km2,pib2;
+
+    printf ("\nVamos a SEGUNDA carta do SUPER TRUNFO! \n");
+
+    printf ("\nDigite o nome do estado: \n");
+    scanf ("%s", &estado2);
+
+    printf ("Digite o código da carta: \n");
+    scanf ("%s" ,&codigo_carta2);
+
+    printf ("Digite o nome da cidade: \n");
+    scanf ("%s" , &nome_da_cidade2);
+
+    printf ("Digite a população da cidade: \n");
+    scanf ("%d" , &populacao2);
+
+    printf ("Digite a área da cidade em km²: \n"); 
+    scanf("%f", &area_em_km2);
+
+    printf ("Digite o PIB da cidade: \n");
+    scanf ("%f", &pib2);
+
+    printf("Digite o número de pontos turisticos da cidade:\n");
+    scanf ("%d" ,&pontos_turisticos2);
+
+char codigo_carta3 [5],nome_da_cidade3[40],estado3 [50];
+int populacao3 , pontos_turisticos3 ;
+float area_em_km3 , pib3 ;
+
+printf ("\nVamos a TERCEIRA carta do SUPER TRUNFO! \n");
+
+    printf ("\nDigite o nome do estado: \n");
+    scanf ("%s", &estado3);
+
+    printf ("Digite o código da carta: \n");
+    scanf ("%s" , &codigo_carta3);
+
+    printf ("Digite o nome da cidade: \n");
+    scanf ("%s" , &nome_da_cidade3);
+
+    printf ("Digite a população da cidade: \n");
+    scanf ("%d" , &populacao3);
+
+    printf ("Digite a área da cidade em km²: \n"); 
+    scanf("%f", &area_em_km3);
+
+    printf ("Digite o PIB da cidade: \n");
+    scanf ("%f", &pib3);
+
+    printf("Digite o número de pontos turisticos da cidade: \n");
+    scanf ("%d" ,&pontos_turisticos3);
+
+char codigo_carta4 [5],nome_da_cidade4[40],estado4 [50];
+int populacao4 , pontos_turisticos4 ;
+float area_em_km4 , pib4 ;
+
+    printf ("\nVamos a QUARTA carta do SUPER TRUNFO! \n");
+
+    printf ("\nDigite o nome do estado: \n");
+    scanf ("%s", &estado4);
+
+    printf ("Digite o código da carta: \n");
+    scanf ("%s" , &codigo_carta4);
+
+    printf ("Digite o nome da cidade: \n");
+    scanf ("%s" , &nome_da_cidade4);
+
+    printf ("Digite a população da cidade: \n");
+    scanf ("%d" , &populacao4);
+
+    printf ("Digite a área da cidade em km²: \n"); 
+    scanf("%f", &area_em_km4);
+
+    printf ("Digite o PIB da cidade: \n");
+    scanf ("%f", &pib4);
+
+    printf("Digite o número de pontos turisticos da cidade: \n");
+    scanf ("%d" ,&pontos_turisticos4);
+
+
+printf ("\nCarta 1: \n");
+    printf ("Estado: %s \n", estado);
+    printf("Código da carta: %s \n", codigo_carta);
+    printf ("Nome da cidade: %s \n" , nome_da_cidade);
+    printf("População: %d \n" , populacao);
+    printf ("Área em km²: %f km²\n", area_em_km);
+    printf ("PIB: %f Bilhões de Reais \n", pib);
+    printf ("Número de pontos turisticos: %d \n\n" , pontos_turisticos);
+
+printf ("Carta 2: \n");
+    printf ("Estado: %s \n", estado2);
+    printf("Código da carta: %s \n", codigo_carta2);
+    printf ("Nome da cidade: %s \n" , nome_da_cidade2);
+    printf("População: %d \n" , populacao2);
+    printf ("Área em km²: %f km²\n", area_em_km2);
+    printf ("PIB: %f Bilhões de Reais \n", pib2);
+    printf ("Número de pontos turisticos: %d \n\n" , pontos_turisticos2);
+
+printf ("Carta 3: \n");
+    printf ("Estado: %s \n", estado3);
+    printf("Código da carta: %s \n", codigo_carta3);
+    printf ("Nome da cidade: %s \n" , nome_da_cidade3);
+    printf("População: %d \n" , populacao3);
+    printf ("Área em km²: %f km²\n", area_em_km3);
+    printf ("PIB: %f Bilhões de Reais \n", pib3 );
+    printf ("Número de pontos turisticos: %d \n\n" , pontos_turisticos3);
+
+printf ("Carta 4: \n");
+    printf ("Estado: %s \n", estado4);
+    printf("Código da carta: %s \n", codigo_carta4);
+    printf ("Nome da cidade: %s \n" , nome_da_cidade4);
+    printf("População: %d \n" , populacao4);
+    printf ("Área em km²: %f km²\n", area_em_km4);
+    printf ("PIB: %f Bilhões de Reais \n", pib4);
+    printf ("Número de pontos turisticos: %d \n\n" , pontos_turisticos4);
+
+
+/*DADOS USADOS COMO MODELO PARA GERAÇÃO DAS CARTAS
+
+Carta 1
+Estado: Ceara
+Código da carta: A01
+Nome da cidade: Fortaleza
+População: 2428678
+Área em Km²: 313.80 Km²
+PIB: 73.80 Bilhões de Reais
+Numero de Pontos Turisticos: 15
+
+Carta 2
+Estado: Bahia
+Código da carta: B01
+Nome da cidade: Salvador
+População: 2417678
+Área em Km²: 693.80 km²
+PIB: 62.95 Bilhões de Reais
+Numero de Pontos Turisticos: 20
+
+Carta 3
+Estado: Pernambuco
+Código da carta: C01
+Nome da cidade: Recife
+População: 9562007
+Área em Km²: 98.312 km²
+PIB: 54.970 Bilhões de Reais
+Numero de Pontos Turisticos: 15
+
+Carta 4
+Estado: Alagoas
+Código da carta: D01
+Nome da cidade: Maceió
+População: 957916
+Área em Km²: 509.6 km²
+PIB: 27.484 Bilhões de Reais
+Numero de Pontos Turisticos: 20
+
+*/
+
+return 0;
+
+
+}
